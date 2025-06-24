9169679ab3b973083e70dfb990a9287a735e28d1
shivraj pandit <panditshivraj934@gmail.com>
2025-06-24 16:08:32 +0530

    by using loop

---
+++ loop.dart
@@ -0,0 +1,23 @@
+void main() {
+  // For loop
+  print('Using for loop:');
+  for (int i = 1; i <= 5; i++) {
+    print('Number: $i');
+  }
+
+  // While loop
+  print('\nUsing while loop:');
+  int j = 1;
+  while (j <= 5) {
+    print('Number: $j');
+    j++;
+  }
+
+  // Do-while loop
+  print('\nUsing do-while loop:');
+  int k = 1;
+  do {
+    print('Number: $k');
+    k++;
+  } while (k <= 5);
+}
